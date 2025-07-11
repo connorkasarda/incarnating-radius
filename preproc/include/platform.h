6330444c45fcada7843c7bd586d53bff4238ce48
Connor <connorkasarda@gmail.com>
2025-07-11 18:08:00 -0500

    added windows, linux, apple detection

--- preproc/include/platform.h
+++ preproc/include/platform.h
@@ -1,6 +1,26 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#if defined(_WIN32) || defined(_WIN64)
+    #define PLATFORM_WINDOWS 1
+#else
+    #define PLATFORM_WINDOWS 0
+#endif
 
+#if defined(__linux__)
+    #define PLATFORM_LINUX 1
+#else
+    #define PLATFORM_LINUX 0
+#endif
+
+#if defined(__APPLE__) && defined(__MACH__)
+    #define PLATFORM_MAC 1
+#else
+    #define PLATFORM_MAC 0
+#endif
+
+#if !PLATFORM_WINDOWS && !PLATFORM_LINUX && !PLATFORM_MAC
+    #error "PLATFORM UNKNOWN"
+#endif
 
 #endif
