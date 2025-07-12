810b5df02c3ebc6d03698f478a588ecaa5b5a47f
Connor <connorkasarda@gmail.com>
2025-07-11 19:57:04 -0500

    reworked format for preprocs

--- preproc/include/platform.h
+++ preproc/include/platform.h
@@ -1,31 +1,24 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#define PLATFORM_WINDOWS 0
+#define PLATFORM_LINUX   0
+#define PLATFORM_MAC     0
+#define PLATFORM_UNIX    0
+
 #if defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__)
+    #undef PLATFORM_WINDOWS
     #define PLATFORM_WINDOWS 1
-#else
-    #define PLATFORM_WINDOWS 0
-#endif
-
-#if defined(__linux__)
+#elif defined(__linux__)
+    #define PLATFORM_LINUX
     #define PLATFORM_LINUX 1
-#else
-    #define PLATFORM_LINUX 0
-#endif
-
-#if defined(__APPLE__) && defined(__MACH__)
+#elif defined(__APPLE__) && defined(__MACH__)
+    #undef PLATFORM_MAC
     #define PLATFORM_MAC 1
-#else
-    #define PLATFORM_MAC 0
-#endif
-
-#if defined(__unix__) || defined(__unix) || PLATFORM_MAC || PLATFORM_LINUX
+#elif defined(__unix__) || defined(__unix) || PLATFORM_MAC || PLATFORM_LINUX
+    #undef PLATFORM_UNIX
     #define PLATFORM_UNIX 1
 #else
-    #define PLATFORM_UNIX 0
-#endif 
-
-#if !PLATFORM_WINDOWS && !PLATFORM_LINUX && !PLATFORM_MAC && !PLATFORM_UNIX
     #error "PLATFORM UNKNOWN"
 #endif
 
