3c11b08d565cae40b574f9e2babc405088164bae
Connor <connorkasarda@gmail.com>
2025-07-11 19:40:23 -0500

    check for well known architectures. some platform detection edits.

--- preproc/include/platform.h
+++ preproc/include/platform.h
@@ -1,7 +1,7 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
-#if defined(_WIN32) || defined(_WIN64)
+#if defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__)
     #define PLATFORM_WINDOWS 1
 #else
     #define PLATFORM_WINDOWS 0
@@ -19,7 +19,13 @@
     #define PLATFORM_MAC 0
 #endif
 
-#if !PLATFORM_WINDOWS && !PLATFORM_LINUX && !PLATFORM_MAC
+#if defined(__unix__) || defined(__unix) || PLATFORM_MAC || PLATFORM_LINUX
+    #define PLATFORM_UNIX 1
+#else
+    #define PLATFORM_UNIX 0
+#endif 
+
+#if !PLATFORM_WINDOWS && !PLATFORM_LINUX && !PLATFORM_MAC && !PLATFORM_UNIX
     #error "PLATFORM UNKNOWN"
 #endif
 
