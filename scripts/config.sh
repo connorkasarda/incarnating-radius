b1c39362f8d91811791546f1f8905aaaada1d3aa
Connor <connorkasarda@gmail.com>
2025-07-17 19:42:38 -0500

    added first bash scripts.

---
+++ scripts/config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# TODO Place in a script file that can be loaded later to reduce reptition
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "$SCRIPTS_DIR/.." && pwd)"
+
+# TODO Configuration step via CMake
