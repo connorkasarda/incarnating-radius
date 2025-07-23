1c9c8000fc290e924c7fabce93a1290f65643c23
Connor <connorkasarda@gmail.com>
2025-07-22 20:03:57 -0500

    start bump allocator. add method to interface.

---
+++ memalloc/include/bump_allocator.h
@@ -0,0 +1,23 @@
+#ifndef BUMP_ALLOCATOR_H
+#define BUMP_ALLOCATOR_H
+
+namespace IncarnatingRadius::Memory
+{
+    class BumpAllocator : public MemoryAllocator
+    {
+    public:
+        explicit BumpAllocator(size_t size);
+        ~BumpAllocator() override;
+        void* allocate(size_t size, size_t alignment) override;
+        template<typename T>
+        T* allocate() override;
+        void deallocate(void* ptr);
+        void reset() override;
+    private:
+        uint8_t* m_start = nullptr;
+        size_t m_capacity = 0;
+        size_t m_offset = 0;
+    }; // class BumpAllocator
+} // namespace IncarnatingRadius::Memory
+
+#endif
--- memalloc/include/memory_allocator.h
+++ memalloc/include/memory_allocator.h
@@ -5,12 +5,27 @@
 
 namespace IncarnatingRadius::Memory
 {
+    /// Interface for memory allocators
     class MemoryAllocator
     {
     public:
+        /// Destructor
         virtual ~MemoryAllocator() = default;
+        /**
+         * @brief Returns pointer to memory for object
+         * @param size Amount of memory required for object
+         * @param alignment Memory alignment required for object
+         * @return Pointer to allocated memory
+         */
         virtual void* allocate(size_t size,
                                size_t alignment = alignof(std::max_align_t)) = 0;
+        /**
+         * @brief Templated allocate that simplifies memory acquisition
+         * @return Pointer to allocated memory for object of type T
+         */
+        template<typename T>
+        virtual T* allocate() = 0;
+        /// Resets entire memory block
         virtual void reset() = 0;
     }; // class MemoryAllocator
 } // namespace IncarnatingRadius::Memory
